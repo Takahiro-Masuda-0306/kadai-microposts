6e3aae079f326bf4d16ad0ea1e9897561c9e5323
EC2 Default User <ec2-user@ip-172-31-80-131.ec2.internal>
2020-04-11 17:09:16 +0000

    user login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts/app')
+
+@section('content')
+
+<div class="text-center">
+  <h1>Log in</h1>
+</div>
+
+<div class="row">
+  <div class="col-sm-6 offset-sm-3">
+    {!! Form::open(['route' => 'login.post']) !!}
+    
+      <div class="form-group">
+      {!! Form::label('email', 'Email') !!}
+      {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+      </div>
+      
+      <div class="form-group">
+      {!! Form::label('password', 'Password') !!}
+      {!! Form::password('password', ['class' => 'form-control']) !!}
+      </div>
+      
+      {!! Form::submit('ログイン', ['class'=>'btn btn-success btn-block']) !!}
+    
+    {!! Form::close() !!}
+  </div>
+  
+  {{ link_to_route('
+</div>
+
+@endsection
